27f96620c1063a5c6b2dd526306cde0d21404fe9
Jessica Deaton <jessicadeaton@outlook.com>
2019-02-25 12:04:22 -0500

    added base template and created basic pages

--- jessicadeaton/urls.py
+++ jessicadeaton/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('posts', views.post_list, name='post_list'),
+	path('now', views.now, name='now'),
+	path('about', views.about, name='about'),
+
+
 
 ]
--- jessicadeaton/views.py
+++ jessicadeaton/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'jessicadeaton/index.html', {})
-	
+
 def post_list(request):
 	return render(request, 'jessicadeaton/post_list.html', {})
+
+def now(request):
+	return render(request, 'jessicadeaton/now.html', {})
+
+def about(request):
+	return render(request, 'jessicadeaton/about.html', {})
