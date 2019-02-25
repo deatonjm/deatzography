4b965f313e1c690f3e01c85856bb05ff10acbb57
Jessica Deaton <jessicadeaton@outlook.com>
2019-02-25 11:51:46 -0500

    added very light templates and url nav

--- jessicadeaton/views.py
+++ jessicadeaton/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+	return render(request, 'jessicadeaton/index.html', {})
+	
+def post_list(request):
+	return render(request, 'jessicadeaton/post_list.html', {})
